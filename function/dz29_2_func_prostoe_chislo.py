e07592d56e393489c01189a678eadf27737b7fa0
User <batman45@yandex.ru>
2022-05-09 15:42:52 +0300

    функции

---
+++ function/dz29_2_func_prostoe_chislo.py
@@ -0,0 +1,15 @@
+# Написать функцию is_prime, принимающую 1 аргумент — число от 0 до 1000,
+# и возвращающую True, если оно простое, и False - иначе.
+
+import random
+
+def is_prime(a):
+    if a < 2:
+        return False
+    print(a)
+    for i in range(2, int(a ** 1/2 + 1)):
+        if a % i == 0:
+            return False
+    else:
+        return True
+print(is_prime(random.randint(0,1000)))
