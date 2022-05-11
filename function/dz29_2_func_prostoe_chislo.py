2e20f409488dacc1d5747511f45143ed3e1c906f
Yaroslav Kabinetskiy <yarikkabanskiy@mail.ru>
2022-05-11 23:31:32 +0300

    Update dz29_2_func_prostoe_chislo.py

--- function/dz29_2_func_prostoe_chislo.py
+++ function/dz29_2_func_prostoe_chislo.py
@@ -7,7 +7,7 @@ def is_prime(a):
     if a < 2:
         return False
     print(a)
-    for i in range(2, int(a ** 1/2 + 1)):
+    for i in range(2, int(a ** 1/2 + 1)): #стпепень 0,5
         if a % i == 0:
             return False
     else:
