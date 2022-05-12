7192ecff6bc407485a69416a4f4dc98d5e8cfc87
Yaroslav Kabinetskiy <yarikkabanskiy@mail.ru>
2022-05-12 21:59:13 +0300

    Update bott.py

--- bott.py
+++ bott.py
@@ -2,6 +2,7 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
 
+
 def hello(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
