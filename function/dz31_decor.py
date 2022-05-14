f80ed09ae12782fd38781065aaee84e90284a259
User <batman45@yandex.ru>
2022-05-14 13:52:47 +0300

    каждый подсчет

--- function/dz31_decor.py
+++ function/dz31_decor.py
@@ -1,9 +1,13 @@
 # Напишите декоратор, который будет считать, сколько раз была вызвана декорируемая функция
 
 def deco(fn):
+    global skoko_raz
+    skoko_raz += 1
     print('nachalo')
     fn()
     print('konets')
+    print(skoko_raz)
+skoko_raz = 0
 
 @deco
 def test1():
@@ -12,3 +16,7 @@ def test1():
 @deco
 def test2():
     print('testim2')
+
+@deco
+def test3():
+    print('testim3')
