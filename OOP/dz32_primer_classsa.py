e1a80d75797dc8ee9097a4a3394244d4b79a5ea2
User <batman45@yandex.ru>
2022-06-07 01:35:21 +0300

    классdz

---
+++ OOP/dz32_primer_classsa.py
@@ -0,0 +1,11 @@
+# Напишите пример любого класса и создайте его объект
+
+class Terminator:
+    def print(self):
+        print('bazuka')
+
+t800 = Terminator
+print(t800)
+
+t800_101 = Terminator()
+t800_101.print()
