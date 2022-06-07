ff2b4de2f3994f2ab51cf44cc05320a694264334
User <batman45@yandex.ru>
2022-06-07 20:44:15 +0300

    классdz_atributy

---
+++ OOP/dz34_atributy.py
@@ -0,0 +1,16 @@
+# Допишите несколько атрибутов в класс с прошлого задания, продемонстрируйте их работу
+
+class Terminator:
+    def print(self):
+        print('bazuka')
+    def dict(self):
+        print('Arni')
+    def str(self):
+        print('bazuka')
+
+print(dir(Terminator))
+t800 = Terminator
+# print(t800)
+#
+# t800_101 = Terminator()
+# t800_101.print()
