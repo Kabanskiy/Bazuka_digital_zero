32137183b5924d474a8760be02413e9ba1b882a2
User <batman45@yandex.ru>
2022-06-09 02:45:18 +0300

    oopractika Hu,an

---
+++ OOPractica/oopract_Human.py
@@ -0,0 +1,27 @@
+class Human:
+    default_name = 'No name'
+    default_age = 0
+
+    def __init__(self, name=default_name, age=default_age):
+        self.name = name
+        self.age = age
+        self.__money = 0
+        self.__house = None
+
+    def info(self):
+        print(f'Name: {self.name}')
+        print(f'Age: {self.age}')
+        print(f'Money: {self.__money}')
+        print(f'House: {self.__house}')
+
+    @staticmethod
+    def default_info():
+        print(f'Default name: {Human.default_name}')
+        print(f'Default age: {Human.default_age}')
+
+    def earn_money(self, amount):
+        self.__money += amount
+        print(f'Заработано: {amount}. У вас {self.__money} денег')
+
+# тесты
+if __name__=='__main__': # значит дальше идут тесты
