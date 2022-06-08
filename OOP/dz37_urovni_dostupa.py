21d39ed1615a8e509f3dd50e329fb09bfaee63e5
User <batman45@yandex.ru>
2022-06-08 23:22:37 +0300

    urovni dostupa dz

---
+++ OOP/dz37_urovni_dostupa.py
@@ -0,0 +1,9 @@
+class Diareya:
+    chair = 'Это нормальный стул'
+    _chair = 'Это жидкий стул'
+    __chair = 'А это совсем жидкий стул, мы не будем его показывать'
+
+ex = Diareya()
+print(ex.chair)
+print(ex._chair)
+print(ex._Diareya__chair)
