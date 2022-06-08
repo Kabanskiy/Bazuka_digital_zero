41312c1e3a441d0c0932f8372094dad8bff48670
User <batman45@yandex.ru>
2022-06-08 14:05:56 +0300

    method classa

--- OOP/36_method_classa.py
+++ OOP/36_method_classa.py
@@ -1,8 +1,13 @@
 
 class Batya():
-    @staticmethod
+    @staticmethod # декоратор
     def staticmethod():
         print('static method called')
 
+class son_Batya(Batya):
+    pass
+
 my_obj = Batya()
 my_obj.staticmethod()
+my_obj2 = son_Batya()
+my_obj2.staticmethod()
