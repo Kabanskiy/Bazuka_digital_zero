183d5e8d341432eb1caecd2f73696e01274b54a7
User <batman45@yandex.ru>
2022-06-08 23:13:49 +0300

    urovni dostupa

--- OOP/37_urovni_dostupa.py
+++ OOP/37_urovni_dostupa.py
@@ -1,8 +1,9 @@
 class Name:
-    a = 1
-    _b = 2
-    __c = 3
+    a = 1 # public
+    _b = 2 # protected
+    __c = 3 # private
 
 ex = Name()
 print(ex.a)
 print(ex._b)
+print(ex._Name__c) # специальный синтаксис для приватного
