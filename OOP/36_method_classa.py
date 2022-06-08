a1852b46002e83ab96b50835a2497c5f5da6d277
User <batman45@yandex.ru>
2022-06-08 14:24:46 +0300

    methody classa3

--- OOP/36_method_classa.py
+++ OOP/36_method_classa.py
@@ -1,13 +1,22 @@
+#
+# class Batya():
+#     @staticmethod # декоратор
+#     def staticmethod():
+#         print('static method called')
+#
+# class son_Batya(Batya):
+#     pass
+#
+# my_obj = Batya()
+# my_obj.staticmethod()
+# my_obj2 = son_Batya()
+# my_obj2.staticmethod()
 
-class Batya():
-    @staticmethod # декоратор
-    def staticmethod():
-        print('static method called')
-
-class son_Batya(Batya):
-    pass
-
-my_obj = Batya()
-my_obj.staticmethod()
-my_obj2 = son_Batya()
-my_obj2.staticmethod()
+class Person():
+    @staticmethod
+    def is_adult(age):
+        if age > 18:
+            return True
+        else:
+            return False
+print(Person.is_adult(19))
