be10d776009d3f9606a141370ff6ab773f04fc34
User <batman45@yandex.ru>
2022-06-09 14:15:13 +0300

    oopractika Human2

---
+++ OOPractica/oopract_Human2.py
@@ -0,0 +1,8 @@
+
+from oopract_Human import Human
+
+# здесь будет работать код, импортированный из файла oopract_Human. Можно подставлять новые данные
+
+Chuck_Norris = Human('Чак', 77)
+Chuck_Norris.info()
+
