d87857bfa66984d076b15dff0c84f0465ae5458f
User <batman45@yandex.ru>
2022-06-10 22:00:10 +0300

    dz_polimorfizm2

---
+++ OOP/dz39-poliforf2.py
@@ -0,0 +1,10 @@
+class Rectangle:
+    def area(self, a, b=None):
+        if b is not None:
+            print('Получился прямоугольник', a * b)
+        else:
+            print('Получился квадрат', a)
+
+
+rectangle_a = Rectangle()
+rectangle_a.area(10)
