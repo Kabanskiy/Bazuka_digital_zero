90a7e8f937bf0bd63effebf4cbcc15ddb186e4f6
User <batman45@yandex.ru>
2022-07-08 14:40:16 +0300

    Добавление примера - класса Class1

--- OOP/35_sv-va_classa.py
+++ OOP/35_sv-va_classa.py
@@ -21,3 +21,26 @@ print(car_object.default_color)
 print(car_object.color)
 
 print(dir(Car))
+
+
+class Class1(object):
+
+    def __init__(self, value):
+        self.__var = value
+
+    def getVar(self):  # Чтение
+        return self.__var
+
+    def setVar(self, value):  # Запись
+        self.__var = value
+
+    def delVar(self):  # Удаление
+        del self.__var
+
+    v = property(getVar, setVar, delVar, "Строка документирования")
+
+c1 = Class1(5)
+
+c1.v = 35  # Вызывается метод setVar()
+print(c1.v)  # Вызывается метод getVar()
+del c1.v  # Вызывается метод delVar()
