69e92886fd4caa85608c2f4d1726fb80ce068a0b
User <batman45@yandex.ru>
2022-07-11 16:04:56 +0300

    исправлен комментарий

--- OOP/38_nasledovanie3.py
+++ OOP/38_nasledovanie3.py
@@ -17,8 +17,3 @@ cell_phone_a = CellPhone()
 cell_phone_a.camera_method()
 cell_phone_a.radio_method()
 
-
-car_a = Car()
-car_a.vehicle_method()  # вызов метода родительского класса
-car_b = Cycle()
-car_b.vehicle_method()  # вызов метода родительского класса
