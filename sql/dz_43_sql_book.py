70291918c261dacd9f9f6b2f2bce253483dd0ee9
User <batman45@yandex.ru>
2022-07-18 21:04:40 +0300

    добавлена строка внесения и цикл

--- sql/dz_43_sql_book.py
+++ sql/dz_43_sql_book.py
@@ -7,12 +7,16 @@ import sqlite3
 
 conn = sqlite3.connect('book.db')
 cursor = conn.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXISTS tab_1(id INTEGER PRIMARY KEY AUTOINCREMENT, col_1 TEXT,col_2 TEXT, INTEGER) ''')
-cursor.execute('''INSERT INTO tab_1(col_1,col_2) VALUES ('book_id','INT PRIMARY KEY AUTO_INCREMENT'),
-INSERT INTO tab_2(col_1,col_2) VALUES ('title','VARCHAR(50)'),
-INSERT INTO tab_3(col_1,col_2) VALUES ('author','VARCHAR(30)'),
-INSERT INTO tab_4(col_1,col_2) VALUES ('price','DECIMAL(8, 2)'),
-INSERT INTO tab_5(col_1,col_2) VALUES ('amount','INT'),
-INSERT INTO tab_6(col_1,col_2) VALUES ('Chuck','Norris')''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS book(id INTEGER PRIMARY KEY AUTOINCREMENT, col_1 TEXT,col_2 TEXT)''')
+cursor.execute('''INSERT INTO book(col_1,col_2) VALUES ('book_id','INT PRIMARY KEY AUTO_INCREMENT'),
+('title','VARCHAR(50)'), ('author','VARCHAR(30)'), ('price','DECIMAL(8, 2)'), ('amount','INT')''')
+cursor.execute("INSERT INTO book(col_1,col_2) VALUES ('Chuck','Norris')") # вносим новую строку
 conn.commit()
 
+cursor.execute('''SELECT*FROM book''')
+k = cursor.fetchall()
+# print(k)
+for i in k:
+    i = list(i) # преобразовали в список
+    h = 0
+    print(' '.join(str(h) for h in i))
